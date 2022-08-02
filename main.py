68c79848a1ab8cca1b15860680d0e08059501e14
ndigre82 <n.digregorio@ibill.tech>
2022-08-02 20:11:53 +0200

    fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path=f'chromedrivers/{"linux" if "linux" in platform else "mac"}_chromedriver')
 driver.get("http://www.python.org")
-# elem = driver.find_element(By.NAME, "q")
+elem = driver.find_element(By.NAME, "q")
 # elem.clear()
 # elem.send_keys("pycon")
 # elem.send_keys(Keys.RETURN)
