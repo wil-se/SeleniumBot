728bce3d0d40968c961e26072b3df6d5b6ceb81f
William <sebastiani.1753672@studenti.uniroma1.it>
2022-08-02 20:02:06 +0200

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+from sys import platform
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+
+driver = webdriver.Chrome(executable_path=f'chromedrivers/{"linux" if "linux" in platform else "mac"}_chromedriver')
+driver.get("http://www.python.org")
+# elem = driver.find_element(By.NAME, "q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+driver.close()
